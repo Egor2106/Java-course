f877a72dc435347200c7577f0d4ff8501e01f1e6
Egor2106 <86969683+Egor2106@users.noreply.github.com>
2022-01-11 22:18:04 +0400

    Add files via upload

--- 2.4/Ages/src/Main.java
+++ 2.4/Ages/src/Main.java
@@ -8,61 +8,36 @@ public class Main {
         int middle = -1;
         int max = -1;
 
-        if (vasyaAge != katyaAge && vasyaAge != mishaAge && katyaAge != mishaAge) {
-
-            if (vasyaAge > katyaAge && vasyaAge > mishaAge)
-                max = vasyaAge;
-            else if ((vasyaAge > katyaAge && vasyaAge < mishaAge) || (vasyaAge < katyaAge && vasyaAge > mishaAge))
-                middle = vasyaAge;
-            else
-                min = vasyaAge;
-
-            if (katyaAge > vasyaAge && katyaAge > mishaAge)
-                max = katyaAge;
-            else if ((katyaAge > vasyaAge && katyaAge < mishaAge) || (katyaAge < vasyaAge && katyaAge > mishaAge))
+        if (vasyaAge > katyaAge && vasyaAge > mishaAge) {
+            max = vasyaAge;
+            if (katyaAge > mishaAge) {
                 middle = katyaAge;
-            else
-                min = katyaAge;
-
-            if (mishaAge > katyaAge && mishaAge > vasyaAge)
-                max = mishaAge;
-            else if ((mishaAge > katyaAge && mishaAge < vasyaAge) || (mishaAge < katyaAge && mishaAge > vasyaAge))
-                middle = mishaAge;
-            else
                 min = mishaAge;
+            } else {
+                middle = mishaAge;
+                min = katyaAge;
+            }
+        } else if (vasyaAge < katyaAge && vasyaAge < mishaAge) {
+            min = vasyaAge;
+            if (katyaAge > mishaAge) {
+                max = katyaAge;
+                middle = mishaAge;
+            } else {
+                max = mishaAge;
+                middle = katyaAge;
+            }
         } else {
-            if (vasyaAge == katyaAge && vasyaAge > mishaAge) {
-                min = mishaAge;
-                middle = vasyaAge;
+            middle = vasyaAge;
+            if (katyaAge > mishaAge) {
                 max = katyaAge;
-            } else if (vasyaAge == katyaAge && vasyaAge < mishaAge){
-                min = vasyaAge;
-                middle = katyaAge;
+                min = mishaAge;
+            } else {
                 max = mishaAge;
-            } else if (vasyaAge == mishaAge && vasyaAge > katyaAge) {
                 min = katyaAge;
-                middle = vasyaAge;
-                max = mishaAge;
-            } else if (vasyaAge == mishaAge && vasyaAge < katyaAge){
-                min = vasyaAge;
-                middle = mishaAge;
-                max = katyaAge;
-            } else if (mishaAge == katyaAge && vasyaAge > mishaAge) {
-                min = mishaAge;
-                middle = katyaAge;
-                max = vasyaAge;
-            } else if (mishaAge == katyaAge && vasyaAge < mishaAge) {
-                min = vasyaAge;
-                middle = katyaAge;
-                max = mishaAge;
-            } else if (mishaAge == katyaAge && vasyaAge == mishaAge) {
-                min = vasyaAge;
-                middle = katyaAge;
-                max = mishaAge;
             }
         }
 
-        System.out.println("Minimal age: " + min + "\nMiddle age: " +
-                middle + "\nMaximum age: " + max);
+        System.out.println("Maximum: " + max + "\nMiddle: " + middle +
+                "\nMinimum: " + min);
     }
 }
